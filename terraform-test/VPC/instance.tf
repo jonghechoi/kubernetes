e84ce86103c3c83d3bc96f509ef67837f27cf2f7
jonghechoi <sosinnmi@naver.com>
2021-05-11 06:36:55 +0900

    2021/05/11

---
+++ terraform-test/VPC/instance.tf
@@ -0,0 +1,54 @@
+
+/*========= Instance =========*/
+resource "aws_instance" "instance_4_control" {
+  ami                  = "ami-0ba348d1c903e5d48"
+  instance_type        = "t2.micro"
+  iam_instance_profile = aws_iam_instance_profile.hello.name
+  associate_public_ip_address = true
+  
+  network_interface {
+    network_interface_id = aws_network_interface.network_interface.id
+    device_index = 0
+  }
+
+  tags = {
+      Name = "instance_4_control"
+  }
+}
+
+resource "aws_iam_role" "iam_role" {
+  name               = "role_4_control_instance"
+  path               = "/"
+
+  tags = {
+      Name = "role_4_control_instance"
+  }
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "*",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "hello" {
+  name = "instance_4_control_profile"
+  role = aws_iam_role.iam_role.name
+}
+
+
+/*========= Instance Network =========*/
+resource "aws_network_interface" "network_interface" {
+    subnet_id = aws_subnet.eks_pub_subnet[0]
+
+    tags = {
+      Name = "network_interface_4_control_instance"
+    }
+}
